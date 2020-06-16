98d21aaa5c26b19fc44e4cca3c03d380c6b6df67
lishijun <lishijun@leyoai.com>
2020-06-16 18:41:08 +0800

    修改引入方式

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,1 @@
-import VueWaterfallMedia from './vue-waterfall-media.vue';
-
-export default {
-    install(Vue) {
-        if (typeof window !== 'undefined' && window.Vue) {
-            window.Vue.component(VueWaterfallMedia.name, VueWaterfallMedia);
-        } else {
-            Vue.component(VueWaterfallMedia.name, VueWaterfallMedia);
-        }
-    },
-};
+export default './vue-waterfall-media.vue';
