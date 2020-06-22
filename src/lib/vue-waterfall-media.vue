344680799dcec7dc9a4fe1f514f2b3af2b9988d3
lishijun <lishijun@leyoai.com>
2020-06-22 13:47:46 +0800

    测试this

--- src/lib/vue-waterfall-media.vue
+++ src/lib/vue-waterfall-media.vue
@@ -205,11 +205,11 @@ export default {
         },
         /* 触底事件防抖 */
         debounceReachBottom() {
-            debounce(this.$options.methods.reachBottom, this.toupperInterval);
+            debounce(this.reachBottom, this.toupperInterval);
         },
         /* 窗口重置防抖 */
         debounceWindowResize() {
-            debounce(this.$options.methods.calcPosition, this.resizeInterval);
+            debounce(this.calcPosition, this.resizeInterval);
         },
     },
 };
