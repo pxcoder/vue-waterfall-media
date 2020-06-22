650de719fd86a3585fe05bab526869e3213afaf9
lishijun <lishijun@leyoai.com>
2020-06-22 11:59:28 +0800

    测试方法找不到bug

--- src/lib/vue-waterfall-media.vue
+++ src/lib/vue-waterfall-media.vue
@@ -205,11 +205,11 @@ export default {
         },
         /* 触底事件防抖 */
         debounceReachBottom() {
-            debounce(this.reachBottom, this.toupperInterval);
+            debounce(this.$options.methods.reachBottom, this.toupperInterval);
         },
         /* 窗口重置防抖 */
         debounceWindowResize() {
-            debounce(this.calcPosition, this.resizeInterval);
+            debounce(this.$options.methods.calcPosition, this.resizeInterval);
         },
     },
 };
